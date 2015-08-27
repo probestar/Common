19b2808d51f2d48656c437242ef75c706fce8d87
ProbeStar <probestar@qq.com>
2015-08-27 16:50:27 +0800

    Add loadProperties in PSUtils.

--- psutils/src/main/java/com/probestar/psutils/PSUtils.java
+++ psutils/src/main/java/com/probestar/psutils/PSUtils.java
@@ -16,10 +16,15 @@
 
 package com.probestar.psutils;
 
+import java.io.FileInputStream;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.UUID;
 
 public class PSUtils {
+	private static PSTracer _tracer = PSTracer.getInstance(PSUtils.class);
+
 	public static ByteBuffer getUUIDBuffer() {
 		ByteBuffer buffer = ByteBuffer.allocate(16);
 		UUID uuid = UUID.randomUUID();
@@ -27,8 +32,24 @@ public class PSUtils {
 		buffer.putLong(uuid.getMostSignificantBits());
 		return buffer;
 	}
-	
+
 	public static byte[] getUUIDArray() {
 		return getUUIDBuffer().array();
+	}
+
+	public static HashMap<String, String> loadProperties(String fileName) {
+		HashMap<String, String> map = new HashMap<>();
+		try {
+			Properties p = new Properties();
+			p.load(new FileInputStream(fileName));
+			for (Object obj : p.keySet()) {
+				String key = obj.toString();
+				String value = p.getProperty(key);
+				map.put(key, value);
+			}
+		} catch (Throwable t) {
+			_tracer.error("PSUtils.loadProperties error.", t);
+		}
+		return map;
 	}
 }
