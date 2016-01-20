a00abdbb131b13b822d4832c5a6b282c1d27758d
Elvis <elvis@ultrapowersoft.com>
2016-01-20 17:03:42 +0800

    Modify PSFile

--- psutils/src/main/java/com/probestar/psutils/PSFile.java
+++ psutils/src/main/java/com/probestar/psutils/PSFile.java
@@ -5,8 +5,8 @@ import java.io.File;
 import com.google.common.io.Files;
 
 public class PSFile {
-	private static String pictureExtensionName = "jpgpngbmp";
-	private static String movieExtensionName = "movmp4";
+	private static String pictureExtensionName = "jpg,png,bmp,gif";
+	private static String movieExtensionName = "mov,mp4,m4v,3gp";
 
 	public static boolean isPicture(File file) {
 		String extension = Files.getFileExtension(file.getAbsolutePath()).toLowerCase();
